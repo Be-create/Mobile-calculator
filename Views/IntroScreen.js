1c653e447d797b5957f60c700e80468121da7d25
Benazir Sultana <Benazir.Sultana@schoolnetindia.com>
2023-02-07 22:48:13 +0530

    add . introscreen

--- Views/IntroScreen.js
+++ Views/IntroScreen.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import { View,Text, Image } from 'react-native'
 import introgif from '../assets/icons8-calculator-64.png'
+import { StyleSheet } from 'react-native';
 const IntroScreen = () => {
   return (
     <View>
         <Text>IntroScreen</Text>
-        <Image source={introgif} />
+        <Image style={styles.image} source={introgif} />
     </View>
   )
 }
+
+
+
+
+const styles = StyleSheet.create({
+  image : {
+  outerHeight:"100px",
+  innerHeight:"100px"
+  }
+})
 
 export default IntroScreen
